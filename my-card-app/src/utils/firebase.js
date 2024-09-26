23a2dc84b86a3e0dc4b5bb1fdbdf49b5f6dfef2a
leakydishes <79079577+leakydishes@users.noreply.github.com>
2024-09-27 01:16:49 +1000

    Move Firebase config to .env

--- my-card-app/src/utils/firebase.js
+++ my-card-app/src/utils/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore, collection, getDocs, addDoc, doc, getDoc, setDoc, deleteDoc, serverTimestamp } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAp0C_M4KT88D4V3kxYs-RmIP04cRIEX34",
-  authDomain: "deakin-app-web-project.firebaseapp.com",
-  projectId: "deakin-app-web-project",
-  storageBucket: "deakin-app-web-project.appspot.com",
-  messagingSenderId: "719775334290",
-  appId: "1:719775334290:web:bda4557b2cbd172a0f8b4b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig); // Initialize Firebase
